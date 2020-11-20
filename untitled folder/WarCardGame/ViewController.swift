944f66c2d035b062116cfbab2be3259e1c539201
student <student@students-Mac.local>
2020-11-20 01:48:43 -0800

    fix_game

--- untitled folder/WarCardGame/ViewController.swift
+++ untitled folder/WarCardGame/ViewController.swift
@@ -35,34 +35,101 @@ class ViewController: UIViewController {
         var playernumber=generateValue()
         var cpunumber=generateValue()
       
-        repeat{
-        PlayerCard.image = UIImage (imageLiteralResourceName: "card\(playernumber)")
-        CPUCard.image = UIImage (imageLiteralResourceName: "card\(cpunumber)")
-        if playernumber>cpunumber {
+        //while playerscore<20 && cpuscore<20{
+            
+        PlayerCard.image = UIImage (imageLiteralResourceName: "card\(cpunumber)")
+        CPUCard.image = UIImage (imageLiteralResourceName: "card\(playernumber)")
+        /*if playernumber > cpunumber{
+            if playerscore < 19 {
+                playerscore += 1
+                message.text = "Player wins the round"
+                PlayerScore.text = String(playerscore)
+                CPUScore.text = String(cpuscore)
+            }
+            else if playerscore == 19
+            
+            {
+                playerscore += 1
+                PlayerScore.text = String(playerscore)
+                CPUScore.text = String(cpuscore)
+                message.text = "WIN! Click to Deal Button to start a new game"
+                playerscore = 0
+                cpuscore = 0
+           
+        }
+        else if playernumber < cpunumber {
+            if cpuscore < 19
+            {
+                cpuscore += 1
+                message.text = "cpu wins the round"
+                PlayerScore.text = String(playerscore)
+                CPUScore.text = String(cpuscore)
+            }
+            else if cpuscore == 19
+            {
+                cpuscore += 1
+                PlayerScore.text = String(playerscore)
+                CPUScore.text = String(cpuscore)
+                message.text = "GAME OVER! Click to Deal Button to start a new game"
+               playerscore = 0
+               cpuscore = 0
+            }
+           
+        }
+        else {
+            message.text = "draw"
+            PlayerScore.text = String(playerscore)
+            CPUScore.text = String(cpuscore)
+        }*/
+            
+            
+            
+            
+         if playerscore==19 && playernumber>cpunumber{
+            playerscore += 1
+            PlayerScore.text = String(playerscore)
+            CPUScore.text = String(cpuscore)
+                 message.text = "WIN! Click to Deal Button to start a new game"
+            playerscore = 0
+            cpuscore = 0
+         }
+         else if cpuscore==19 && playernumber<cpunumber {
+            cpuscore += 1
+            PlayerScore.text = String(playerscore)
+            CPUScore.text = String(cpuscore)
+             message.text = "GAME OVER! Click to Deal Button to start a new game"
+            playerscore = 0
+            cpuscore = 0
+         }
+            
+         
+        else if (playernumber>cpunumber && playerscore < 19 && cpuscore < 19) {
             playerscore += 1
             message.text = "Player wins the round"
             PlayerScore.text = String(playerscore)
             CPUScore.text = String(cpuscore)
         }
-        else if playernumber<cpunumber{
+        else if playernumber<cpunumber && playerscore<19 && cpuscore<19{
             cpuscore += 1
             message.text = "cpu wins the round"
             PlayerScore.text = String(playerscore)
             CPUScore.text = String(cpuscore)
         }
-        else {
+        else if playernumber == cpunumber {
            
             message.text = "draw"
             PlayerScore.text = String(playerscore)
             CPUScore.text = String(cpuscore)
         }
-        }while playerscore <= 20 || cpuscore <= 20
+        
+   
+       
         
         
         
     
     }
+    }
     
-    
-}
+
 
